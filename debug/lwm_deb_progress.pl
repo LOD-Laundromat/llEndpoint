7011bee1817d11f5ff56f9bb4f11f9654e88954a
Wouter Beek <w.g.j.beek@vu.nl>
2014-09-26 11:44:18 +0200

    doc

--- debug/lwm_deb_progress.pl
+++ debug/lwm_deb_progress.pl
@@ -11,7 +11,7 @@
 A Web-based debug tool for tracking the progress of the LOD Washing Machine.
 
 @author Wouter Beek
-@version 2014/05-2014/06, 2014/08
+@version 2014/05-2014/06, 2014/08-2014/09
 */
 
 :- use_module(library(http/html_write)).
