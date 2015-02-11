a3d9ce3c8fafdd8033e279b573b10978216ec622
Wouter Beek <me@wouterbeek.com>
2015-02-11 16:03:38 +0100

    CLEAN: Simplified HTTP interface for the LOD Basket.

--- basket/basket_web.pl
+++ basket/basket_web.pl
@@ -10,7 +10,6 @@ Web-based front-end to the LOD basket.
 
 :- use_module(library(http/http_cors)).
 :- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_json)).
 :- use_module(library(http/http_parameters)).
 :- use_module(library(semweb/rdf_db), except([rdf_node/1])).
 
@@ -26,19 +25,16 @@ Web-based front-end to the LOD basket.
 
 basket(Request):-
   cors_enable,
-  (   catch(
-        http_parameters(Request, [uri(Uri,[atom])]),
-        _,
-        fail
-      ),
+  memberchk(pool(client(_,_,_,Out)), Request),
+  (   http_parameters(Request, [uri(Uri,[atom])]),
       is_uri(Uri)
   ->  % Make sure that it is a URL.
       add_to_basket(Uri),
       % HTTP status code 202 Accepted: The request has been accepted
       % for processing, but the processing has not been completed.
-      reply_json(json{}, [status(202)])
+      format('~d', [202])
   ;   % HTTP status code 400 Bad Request: The request could not
       % be understood by the server due to malformed syntax.
-      reply_json(json{}, [status(400)])
+      format('~d', [400])
   ).
 
